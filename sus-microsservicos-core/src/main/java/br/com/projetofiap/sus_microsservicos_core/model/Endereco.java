c9802e46a067d65f53d8582870ea58dbd6cd535a
elias pinheiro pereira <eliaspinheiropereiraa@gmail.com>
2026-02-03 19:19:39 -0300

    iniciando a criação de endereco

---
+++ sus-microsservicos-core/src/main/java/br/com/projetofiap/sus_microsservicos_core/model/Endereco.java
@@ -0,0 +1,29 @@
+package br.com.projetofiap.sus_microsservicos_core.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "endereco")
+@Entity
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String rua;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+}
