75888a2a67fab8d5cc5c08bd33a88268742ce7f7
elias pinheiro pereira <eliaspinheiropereiraa@gmail.com>
2026-02-02 21:21:33 -0300

    iniciando projeto

---
+++ sus-microsservicos-core/src/main/java/br/com/projetofiap/sus_microsservicos_core/SusMicrosservicosCoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetofiap.sus_microsservicos_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusMicrosservicosCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusMicrosservicosCoreApplication.class, args);
+	}
+
+}
---
+++ sus-microsservicos-core/src/test/java/br/com/projetofiap/sus_microsservicos_core/SusMicrosservicosCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetofiap.sus_microsservicos_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusMicrosservicosCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
